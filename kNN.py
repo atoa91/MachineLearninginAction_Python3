19f3c286b2ffdd7fbadd609bd84d73154710fbe8
jihoon Lee <frank.jihoon.lee@gmail.com>
2016-12-20 14:07:10 +0900

    hand writing analysis with kNN
    By using 32 x 32 image data with 0,1 to identify handwriting number.

--- kNN.py
+++ kNN.py
@@ -1,5 +1,7 @@
 from numpy import *
+from os import listdir
 import operator
+
 
 def createDataSet():
 	group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
@@ -79,3 +81,29 @@ def img2vector(filename):
 		for j in range(32):
 			returnVect[0, 32*i + j] = int(lineStr[j])
 	return returnVect
+
+#from os import listdir
+def handwritingClassTest():
+	hwLabels = []
+	trainingFileList = listdir('./ch2/digits/trainingDigits')
+	m = len(trainingFileList)
+	trainingMat = zeros((m,1024))
+	for i in range(m):
+		fileNameStr = trainingFileList[i]
+		fileStr = fileNameStr.split('.')[0]
+		classNumStr = int(fileStr.split('_')[0])
+		hwLabels.append(classNumStr)
+		trainingMat[i,:] = img2vector('./ch2/digits/trainingDigits/%s' % fileNameStr)
+	testFileList = listdir('./ch2/digits/testDigits')
+	errorCount = 0.0
+	mTest = len(testFileList)
+	for i in range(mTest):
+		fileNameStr = testFileList[i]
+		fileStr = fileNameStr.split('.')[0]
+		classNumStr = int(fileStr.split('_')[0])
+		vectorUnderTest = img2vector('./ch2/digits/testDigits/%s' % fileNameStr)
+		classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+		print ("the classifier came back with: %d, the real answer is: %d" % (classifierResult, classNumStr))
+		if ( classifierResult != classNumStr): errorCount += 1.0
+	print('\nthe total number of errors is: %d' % errorCount)
+	print('\nthe total error rate is: %f' % (errorCount/float(mTest)))
